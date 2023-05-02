8b1bfb4ac04f517df337a316223e90458190c168
sunday <117924550+sunepa01@users.noreply.github.com>
2023-05-02 12:11:51 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Portfolio from './pages/dashboard/component/Portfolio.jsx';
+function App() {
+  return (
+    <div className="App">
+      <Portfolio/> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/dashboard/componenent/Portfolio.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import '../styles/Portfolio.css'
+
+export function Portfolio() {
+  return (
+    <div className="header">
+      <h1>Portfolio</h1>
+    </div>
+  );
+}
+
+
+export default Portfolio;
